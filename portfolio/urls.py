ee9ef6022e51e903d1b2a249e13749c411ad8a2e
Sage Hogue <Sagehogue@gmail.com>
2018-05-15 14:47:34 -0700

    I made an oops

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -20,6 +20,6 @@ from django.contrib import admin
 from django.urls import path
 
 urlpatterns = [
-    path(r'^admin/', admin.site.urls),
+    path(r'^admin/$', admin.site.urls),
     url(r'', include('blog.urls')),
 ]
