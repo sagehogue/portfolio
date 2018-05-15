7ff12e43108428c4f270833a6a1f051c25479a7f
Sage Hogue <Sagehogue@gmail.com>
2018-05-15 14:25:35 -0700

    oops

--- blog/views.py
+++ blog/views.py
@@ -7,7 +7,7 @@ from django.utils import timezone
 
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
-    context_dict: {
+    context_dict = {
         'posts': posts
     }
     return render(request, 'blog\post_list.html', context=context_dict)
