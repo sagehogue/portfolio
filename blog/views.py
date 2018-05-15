c5c50e55c7efa39b7a9ce7eac65b1f7b31fcaca7
Sage Hogue <Sagehogue@gmail.com>
2018-05-15 16:15:28 -0700

    We did a thing

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Post
 from django.utils import timezone
 
@@ -11,3 +11,7 @@ def post_list(request):
         'posts': posts
     }
     return render(request, 'blog/post_list.html', context=context_dict)
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'posts': post})
