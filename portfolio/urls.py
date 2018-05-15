2e96670158af876cd7c8a2d25a410f127d2bead7
Sage Hogue <Sagehogue@gmail.com>
2018-05-15 12:02:11 -0700

    blogwork

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -1,3 +1,6 @@
+from django.conf.urls import include, url
+from django.contrib import admin
+
 """portfolio URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
@@ -17,5 +20,6 @@ from django.contrib import admin
 from django.urls import path
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path(r'^admin/', admin.site.urls),
+    url(r'', include('blog.urls')),
 ]
