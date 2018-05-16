6b1b653b79b0c717722b62a39bbdf9d201fb5d1f
Sage Hogue <Sagehogue@gmail.com>
2018-05-16 10:39:37 -0700

    Fixing individual post display

--- blog/views.py
+++ blog/views.py
@@ -14,4 +14,4 @@ def post_list(request):
 
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
-    return render(request, 'blog/post_detail.html', {'posts': post})
+    return render(request, 'blog/post_detail.html', {'post': post})
