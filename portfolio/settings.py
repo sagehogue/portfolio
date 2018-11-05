5743870387dc308878c5bb231a886123450cece3
Sage Hogue <Sagehogue@gmail.com>
2018-11-05 07:39:14 -0800

    rest framework tut

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'blog.apps.BlogConfig',
     'story',
     'rest_framework',
+    'snippets.apps.SnippetsConfig',
 ]
 
 REST_FRAMEWORK = {
@@ -47,7 +48,9 @@ REST_FRAMEWORK = {
     # or allow read-only access for unauthenticated users.
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
-    ]
+    ],
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10
 }
 
 MIDDLEWARE = [
