8b4c166ea35cbcfc0e923e72ebf954c71099c665
Sage Hogue <Sagehogue@gmail.com>
2018-11-05 22:12:46 -0800

    continued my exploration of REST framework

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -21,6 +21,8 @@ from django.urls import path
 from rest_framework import routers, serializers, viewsets, views
 
 from . import serializers, views
+from snippets import views as snipviews
+
 
 # Django REST Framework
 
@@ -29,12 +31,16 @@ from . import serializers, views
 router = routers.DefaultRouter()
 router.register(r'users', views.UserViewSet)
 router.register(r'groups', views.GroupViewSet)
+# Gotta use these viewsets I think, that's why the code below didn't work
+# router.register('snippets', snipviews.snippet_list)
+# router.register(r'snippets/<int:pk>/', snipviews.snippet_detail)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     url('blog/', include('blog.urls'), name='blog'),
-    # Wire up our API using automatic URL routing.
+    # Wire up our API using automatic URL routing.g
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    path(r'api/snippets/', include('snippets.urls')),
     url(r'^api/', include(router.urls)),
     url('', include('story.urls')),
 ]
